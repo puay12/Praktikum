e284ce9850e7fcc1b7dcb8e5d7eea656d8cbce29
puay12 <puayyssii12@gmail.com>
2022-03-02 18:24:05 +0700

    hilow

--- prak2_mg3/lib/main.dart
+++ prak2_mg3/lib/main.dart
@@ -24,14 +24,23 @@ class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+        body: SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
-            margin: EdgeInsets.only(top: 16.0),
-            child: Text("Surabaya Submarine Monument"),
+            margin: EdgeInsets.only(top: 72.0),
+            child: Text(
+              "Surabaya Submarine Monument",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
         ],
       ),
-    );
+    ));
   }
 }
