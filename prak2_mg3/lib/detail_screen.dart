2cb4deeb7f5a67cd2b98f4a476119e7d8ece2cd1
puay12 <puayyssii12@gmail.com>
2022-03-02 23:25:09 +0700

    hilow

--- prak2_mg3/lib/detail_screen.dart
+++ prak2_mg3/lib/detail_screen.dart
@@ -12,7 +12,7 @@ class DetailScreen extends StatelessWidget {
         children: <Widget>[
           Image.asset('asset/img/submarine.jpg'),
           Container(
-            margin: EdgeInsets.only(top: 72.0, bottom: 16.0),
+            margin: EdgeInsets.only(top: 24.0, bottom: 16.0),
             child: Text(
               "Surabaya Submarine Monument",
               textAlign: TextAlign.center,
@@ -54,6 +54,30 @@ class DetailScreen extends StatelessWidget {
               "Museum inside a decommissioned Russian war submarine with tours & an adjacent park with cafes. Clean and well maintained. Car park costs 10k, entrance fee 15k/person. You can see KRI Pasopati there. It is a Russian whiskey class. You can also watch the video about the Indonesian Navy at the building beside the submarine.",
               textAlign: TextAlign.left,
               style: TextStyle(fontSize: 16.0),
+            ),
+          ),
+          Container(
+            height: 150,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget> [
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Image.network('https://www.google.com/url?sa=i&url=https%3A%2F%2Fkfmap.asia%2Flandmark%2Fsurabaya-submarine-monument-13183&psig=AOvVaw2nyrcaVTjh5vBRb1c_v3Lf&ust=1646324410659000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJjutr_qp_YCFQAAAAAdAAAAABAD'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Image.asset('asset/img/img1.jpg'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Image.asset('asset/img/img2.jpg'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Image.asset('asset/img/img3.jpg'),
+                ),
+              ],
             ),
           ),
         ],
