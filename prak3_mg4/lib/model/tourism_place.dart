0c75ee2bc4b8d0b165629580491871385cee0398
puay12 <puayyssii12@gmail.com>
2022-03-11 00:40:40 +0700

    hilow

--- prak3_mg4/lib/model/tourism_place.dart
+++ prak3_mg4/lib/model/tourism_place.dart
@@ -2,58 +2,102 @@ class TourismPlace {
   String name;
   String location;
   String imageAsset;
+  String desc;
+  String dayopen;
+  String timeopen;
+  String price;
 
   TourismPlace({
     required this.name,
     required this.location,
     required this.imageAsset,
+    required this.desc,
+    required this.dayopen,
+    required this.timeopen,
+    required this.price,
   });
 }
 
 var tourismPlaceList = [
   TourismPlace(
-    name: "Surabaya Submarine Monument", 
-    location: "Jl. Pemuda", 
+    name: "Surabaya Submarine Monument",
+    location: "Jl. Pemuda",
     imageAsset: "asset/img/submarine.jpg",
+    desc: "Museum inside a decommissioned Russian war submarine with tours & an adjacent park with cafes. Clean and well maintained. Car park costs 10k, entrance fee 15k/person. You can see KRI Pasopati there. It is a Russian whiskey class. You can also watch the video about the Indonesian Navy at the building beside the submarine.",
+    dayopen: "Everyday",
+    timeopen: "08:00 - 16:00",
+    price: "Rp 10.000,00",
   ),
   TourismPlace(
-    name: "Klenteng Sanggar Agung", 
-    location: "Kenjeran", 
+    name: "Klenteng Sanggar Agung",
+    location: "Kenjeran",
     imageAsset: "asset/img/klenteng.jpg",
+    desc: "Sanggar Agung Temple or Hong San Tang (Chinese: 宏善堂) is a Chinese temple in Surabaya dedicated to Chinese deities and other Asian religious icons. It is located within the Pantai Ria amusement park and has become a tourist destination, even though it is originally a worship place for Tridharma followers. The name of Sanggar Agung is derived from Indonesian language which can be translated as Great Hall.",
+    dayopen: "Mon - Fri",
+    timeopen: "08:00 - 16:00",
+    price: "Rp 12.000,00",
   ),
   TourismPlace(
-    name: "House of Sampoerna", 
-    location: "Krembangan Utara", 
+    name: "House of Sampoerna",
+    location: "Krembangan Utara",
     imageAsset: "asset/img/sampoerna.jpeg",
+    desc: "Open to the public since October 9, 2003, House of Sampoerna (HoS) is a Dutch colonial-style heritage building complex built in 1862 and located in the 'Old Surabaya' area. This complex was initially an orphanage for boys managed by the Dutch. In 1932, it was purchased by Liem Seeng Tee then became the first Sampoerna’s production facility. House of Sampoerna complex consists of a large central auditorium, two smaller buildings in the East and West wings.",
+    dayopen: "Tue - Sun",
+    timeopen: "10:00 - 21:00",
+    price: "Rp 20.000,00",
   ),
   TourismPlace(
-    name: "Tugu Pahlawan", 
-    location: "Alun-alun contong", 
+    name: "Tugu Pahlawan",
+    location: "Alun-alun contong",
     imageAsset: "asset/img/pahlawan.jpg",
+    desc: "The Heroes Monument (Indonesian: Tugu Pahlawan) is a monument in Surabaya, Indonesia. It is the main symbol of the city, dedicated to the people who died during the Battle of Surabaya on 10 November 1945. The 10 November Museum is located under the monument. This monument is 41.15 metres tall and is phallus-shaped. It was built to commemorate the events of 10 November 1945 at the Battle of Surabaya.",
+    dayopen: "Everyday",
+    timeopen: "10:00 - 17:00",
+    price: "Rp 10.000,00",
   ),
   TourismPlace(
-    name: "Patung Suro Boyo", 
-    location: "Wonokromo", 
+    name: "Patung Suro Boyo",
+    location: "Wonokromo",
     imageAsset: "asset/img/patungsby.jpg",
+    desc: "The Heroes Monument (Indonesian: Tugu Pahlawan) is a monument in Surabaya, Indonesia. It is the main symbol of the city, dedicated to the people who died during the Battle of Surabaya on 10 November 1945. The 10 November Museum is located under the monument. This monument is 41.15 metres tall and is phallus-shaped. It was built to commemorate the events of 10 November 1945 at the Battle of Surabaya.",
+    dayopen: "Everyday",
+    timeopen: "Everytime",
+    price: "Free",
   ),
   TourismPlace(
-    name: "Jembatan Suramadu", 
-    location: "Perbatasan Surabaya-Madura", 
+    name: "Jembatan Suramadu",
+    location: "Perbatasan Surabaya-Madura",
     imageAsset: "asset/img/suramadu.jpg",
+    desc: "The Suramadu Bridge (Indonesian: Jembatan Suramadu), also known as the Surabaya–Madura Bridge, is a cable-stayed bridge between Surabaya on the island of Java and the town of Bangkalan on the island of Madura in Indonesia.[4] Opened in June 2009,[5] the 5.4-km bridge is the longest in Indonesia and the second longest in southern hemisphere. It is the first bridge to cross the Madura Strait.[6]",
+    dayopen: "Everyday",
+    timeopen: "Everytime",
+    price: "Free",
   ),
   TourismPlace(
-    name: "Surabaya North Quay", 
-    location: "Terminal Gapura Surya Nusantara", 
+    name: "Surabaya North Quay",
+    location: "Terminal Gapura Surya Nusantara",
     imageAsset: "asset/img/northquay.jpg",
+    desc: "Surabaya North Quay is actually a terminal for luxury cruise ships that dock. However, this place is deliberately addressed so that it can become a tourist spot for residents of Surabaya and its surroundings. In fact, not a few who come from outside the area are curious to enjoy the amazing scenery. The high seas with luxury cruise ships that dock in this terminal, are the main attraction.",
+    dayopen: "Tue - Sun",
+    timeopen: "12:00 - 20:00",
+    price: "Rp 50.000,00",
   ),
   TourismPlace(
-    name: "Jembatan Kenjeran Surabaya", 
-    location: "Kenjeran", 
+    name: "Jembatan Kenjeran Surabaya",
+    location: "Kenjeran",
     imageAsset: "asset/img/kenjeran.jpg",
+    desc: "Suroboyo Bridge or commonly known as Kenjeran Bridge is a through arch bridge across the coastal area of Eastern Surabaya in Kenjeran Beach, District Kenjeran, Surabaya, East Java, Indonesia. This bridge has a length of 800 meters with a width of 18 meters and a height of 12 meters which is held with 150 stakes. The design of the bridge is built in a circle with a view of the fountain in the center of bridge.",
+    dayopen: "Mon - Sat",
+    timeopen: "10:00 - 20:00",
+    price: "Free",
   ),
   TourismPlace(
-    name: "Monumen Jalesveva Jayamahe", 
-    location: "Armada Timur TNI Angkatan Laut", 
+    name: "Monumen Jalesveva Jayamahe",
+    location: "Armada Timur TNI Angkatan Laut",
     imageAsset: "asset/img/jayamahe.jpg",
+    desc: "Jalesveva Jayamahe Monument or can be locally referred to as Monjaya is a statue of an Indonesian Navy officer wearing Ceremonial Service Dress, complete with his sword of honor, staring far to the sea as if he's ready to challenge the ocean's tide and storms. It represents the preparedness of the Indonesian Navy for glory. The statue is standing tall above a building, the total height of the building and the statue reaches 60.6 meters (approximately 198 feet and 9 inches).",
+    dayopen: "Mon - Sat",
+    timeopen: "10:00 - 20:00",
+    price: "Rp 20.000,00",
   ),
 ];
