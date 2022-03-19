67a911182bc86c22872a49f3b1105ddb247cc361
puay12 <puayyssii12@gmail.com>
2022-03-19 08:33:50 +0700

    hilow

--- prak4_mg5_state/lib/done_tourism_list.dart
+++ prak4_mg5_state/lib/done_tourism_list.dart
@@ -25,14 +25,42 @@ class DoneTourismList extends StatelessWidget {
               color: Colors.white60,
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Text(
-                    place.name,
-                    style: const TextStyle(fontSize: 16.0),
+                  Expanded(
+                    flex: 1,
+                    child: Image.asset(place.imageAsset),
                   ),
-                  const Icon(Icons.done_outline),
-                  // Checkbox(value: isDone, onChanged: )
+                  Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget> [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  place.name,
+                                  style: TextStyle(fontSize: 16.0),
+                                ),
+                              ),
+                              Icon(Icons.done_outline),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            place.location,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
