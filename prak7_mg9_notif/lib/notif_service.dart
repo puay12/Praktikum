c6c489c6632213634dfc7aff7c38a4c70564ee1a
puay12 <puayyssii12@gmail.com>
2022-04-17 13:39:39 +0800

    hilow

--- prak7_mg9_notif/lib/notif_service.dart
+++ prak7_mg9_notif/lib/notif_service.dart
@@ -1,10 +1,59 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class NotifService extends StatelessWidget {
+class NotifService extends StatefulWidget {
   const NotifService({Key? key}) : super(key: key);
 
   @override
+  State<NotifService> createState() => _NotifServiceState();
+}
+
+class _NotifServiceState extends State<NotifService> {
+  String messageTitle = "Empty";
+  String notificationAlert = "alert";
+
+  FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _firebaseMessaging.configure(
+      onMessage: (message) async{
+        setState(() {
+          messageTitle = message["notification"]["title"];
+          notificationAlert = "New Notification Alert";
+        });
+      },
+      onResume: (message) async{
+        setState(() {
+          messageTitle = message["data"]["title"];
+          notificationAlert = "Application opened from Notification";
+        });
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Push Notification"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              notificationAlert,
+            ),
+            Text(
+              messageTitle,
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
